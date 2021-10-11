0ea9c0c129bdd17690d1746468f1a01fefd22b04
Animesh Sahu <animeshsahu19@yahoo.com>
2021-10-11 15:10:26 +0530

    Set scaling to 1.25x, add pipewire, some improvement, and some temporary changes in eww.yuck

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,13 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.emacs.d/bin
 fish_add_path /usr/lib/ruby/gems/3.0.0/bin
+fish_add_path ~/Projects/eww/target/release
 
 set fish_greeting
 
 # https://stackoverflow.com/a/42265848/11377112
 set -x GPG_TTY (tty)
+set -x PKG_CONFIG_PATH /usr/lib/pkgconfig
 
 starship init fish | source
 
