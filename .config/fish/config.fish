ff2f0039b1d5d9ce8d6349f8d71f181008842212
Animesh Sahu <animeshsahu19@yahoo.com>
2021-10-21 09:15:50 +0530

    Fix pwd-launch in launching kitty, implement cpu temps, power consumption, brightness and volume in eww bar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.emacs.d/bin
 fish_add_path /usr/lib/ruby/gems/3.0.0/bin
-fish_add_path ~/Projects/eww/target/release
+fish_add_path ~/Projects/RustProjects/eww/target/release
 
 set fish_greeting
 
