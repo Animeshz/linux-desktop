6518bec51461306464c89234c9992a0234c29414
Animesh Sahu <animeshsahu19@yahoo.com>
2022-02-21 00:13:26 +0530

    Add ~/.config/yadm/files and edit script paths in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,11 @@ function fish_user_key_bindings
     #fish_vi_key_bindings --no-erase insert
 end
 
-fish_add_path ~/.scripts
-fish_add_path ~/.scripts/snips
+fish_add_path (fd -td -d1 . ~/.scripts 2>/dev/null || find ~/.scripts -type d -maxdepth 1)
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.emacs.d/bin
 fish_add_path /usr/lib/ruby/gems/3.0.0/bin
-#fish_add_path /opt/balena-cli
 fish_add_path /opt/flutter/bin
 fish_add_path ~/Projects/RustProjects/eww/target/release
 
