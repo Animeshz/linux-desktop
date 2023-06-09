1fc1721555ff71767ac8367ab34cbe43b03d445e
Animesh Sahu <animeshsahu19@yahoo.com>
2023-06-10 04:04:10 +0530

    feat: add brave with extensions and cli-flags

--- homes/x86_64-linux/animesh@framework/default.nix
+++ homes/x86_64-linux/animesh@framework/default.nix
@@ -6,6 +6,7 @@ with lib.internal;
   home.stateVersion = "23.05";
   nix.extraOptions = "max-jobs = auto";
 
+  # TODO: Setup kitty starship fish
   apps.emacs = enabled;
 
   apps.git = {
@@ -31,5 +32,40 @@ with lib.internal;
     ];
 
     # TODO: install gfx drivers with enable.
+  };
+
+  programs.brave = {
+    enable = true;
+    extensions = [
+      { id = "nngceckbapebfimnlniiiahkandclblb"; }  # Bitwarden
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; }  # Dark Reader
+      { id = "jiaopdjbehhjgokpphdfgmapkobbnmjp"; }  # Youtube-shorts block
+      { id = "omkfmpieigblcllmkgbflkikinpkodlk"; }  # enhanced-h264ify
+      { id = "jghecgabfgfdldnmbfkhmffcabddioke"; }  # Volume Master
+      { id = "bpelaihoicobbkgmhcbikncnpacdbknn"; }  # Chrome Regex Search
+      { id = "jabopobgcpjmedljpbcaablpmlmfcogm"; }  # WhatFont
+      { id = "ckkdlimhmcjmikdlpkmbgfkaikojcbjk"; }  # Markdown Viewer
+      # { id = "gpcahfpgfhbghoplpdmjomdemaphldpl";    # Multi-Touch Pinch Zoom
+      #   crxPath = ./multi-touch-pinch-zoom/extension.crx;
+      #   version = "0.95";
+      # }
+    ];
+    commandLineArgs = [
+      "--force-dark-mode"
+      "--enable-features=VaapiVideoEncoder,VaapiVideoDecoder,CanvasOopRasterization,TouchpadOverscrollHistoryNavigation,WebUIDarkMode"
+      "--enable-zero-copy"
+      "--use-gl=desktop"
+      "--ignore-gpu-blocklist"
+      "--enable-oop-rasterization"
+      "--enable-raw-draw"
+      "--enable-gpu-compositing"
+      "--enable-gpu-rasterization"
+      "--enable-native-gpu-memory-buffers"
+      "--use-vulkan"
+      "--disable-features=UseChromeOSDirectVideoDecoder"
+      "--disable-sync-preferences"
+      "--force-device-scale-factor=1.5"
+      "--password-store=basic"
+    ];
   };
 }
